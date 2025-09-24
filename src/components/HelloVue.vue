0f9e14e0c112033576aae532ec25d7af45a00e96
Steven Arycena Fatich <steven.a.fatich@gli.id>
2025-09-24 19:57:42 +0700

    first commit

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,13 @@
+<script setup>
+console.info('Hello Vue 3 + Vite!')
+</script>
+
+<template>
+    <h1 class="hello">Hello Vue 3 + Vite!</h1>
+</template>
+
+<style>
+.hello {
+    color: green;
+}
+</style>
